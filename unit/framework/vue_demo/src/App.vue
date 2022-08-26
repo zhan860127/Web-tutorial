eaf84f5b15f7dd733c2abb20cd638d8a1c5dbfa9
tonyzhan <tonyzhan@vgame.com.tw>
2022-08-26 17:20:30 +0800

    add someting

--- unit/framework/vue_demo/src/App.vue
+++ unit/framework/vue_demo/src/App.vue
@@ -2,11 +2,18 @@
 #app
   #input
     // Step 1.1 code goes here
+    input(v-model="task", type="text", placeholder="Task...")
     // Step 2.1 code goes here
-
+        button(@click='addTask') Add
   // Step 1.2 code goes here
-
+    p {{task}}
   // Step 3.1 code goes here
+.task(v-for="task in tasks")
+  div(@click='toggleTask(task.name)')
+    input(v-model="task.toggle",type="checkbox")
+    label(v-if="!task.toggle") {{ task.name }}
+    label(v-if="task.toggle"): del {{ task.name }}
+
 </template>
 
 <script>
@@ -14,13 +21,26 @@ export default {
   data() {
     return {
       // Step 1.3 code goes here
+            task: 'Data binding is cool!',
+
       // Step 2.2 code goes here
+            tasks: [],
+
     };
   },
 
   methods: {
     // Step 2.3 code goes here
-
+addTask() {
+  if (!this.task)
+    return
+  this.tasks.push({
+    name: this.task,
+    toggle : false
+  })
+  this.task=''
+  console.log(name)
+},
     toggleTask(tsk) {
       const toggle = this.tasks.find((task) => task.name === tsk).toggle;
       this.tasks.find((task) => task.name === tsk).toggle = !toggle;
